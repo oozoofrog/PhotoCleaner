8339808df9d61f35b1e654caa9357e2db3fb0b12
oozoofrog <oozoofrog@gmail.com>
2026-01-01 23:42:01 +0900

    Initial Commit

---
+++ PhotoCleaner/PhotoCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerApp.swift
+//  PhotoCleaner
+//
+//  Created by oozoofrog on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoCleanerTests/PhotoCleanerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerTests.swift
+//  PhotoCleanerTests
+//
+//  Created by oozoofrog on 1/1/26.
+//
+
+import Testing
+@testable import PhotoCleaner
+
+struct PhotoCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
